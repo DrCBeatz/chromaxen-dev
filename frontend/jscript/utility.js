4ecf36501e06cc3a842f3db104910de539e559f9
DrCBeatz <brynecarruthers@gmail.com>
2024-12-08 18:13:01 -0500

    refactor utility.js to ES6 variables & add JSDoc

--- frontend/jscript/utility.js
+++ frontend/jscript/utility.js
@@ -1,8 +1,16 @@
 // jscript/utility.js
 
+/**
+ * Returns a shallow copy of the provided array. This function is useful if you want 
+ * to ensure that modifications to the returned array do not affect the original array.
+ *
+ * @function dereference
+ * @param {Array} array - The array to be copied.
+ * @returns {Array} A new array containing the same elements as the original.
+ */
 export function dereference(array){
-    var new_array = []
-    for(var i = 0; i < array.length; i++){
+    const new_array = []
+    for(let i = 0; i < array.length; i++){
         new_array.push(array[i])
     }
     return new_array
