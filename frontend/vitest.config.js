5ba8bacdebb05214899fcde30aec9de12e439a34
DrCBeatz <brynecarruthers@gmail.com>
2024-12-22 20:43:51 -0500

    add coverage-v8 to package.json & vitest.config.js

--- frontend/vitest.config.js
+++ frontend/vitest.config.js
@@ -5,5 +5,9 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     environment: 'jsdom',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'html', 'lcov']
+    },
   },
 })
