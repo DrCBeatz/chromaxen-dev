30b96d4992b08c463ab4772d7549d774b0f2aa5e
DrCBeatz <brynecarruthers@gmail.com>
2025-01-12 19:50:39 -0500

    add test for makeNewGame() to gamelogic.test.js

--- frontend/jscript/__tests__/gamelogic.test.js
+++ frontend/jscript/__tests__/gamelogic.test.js
@@ -31,7 +31,9 @@ import * as gameUI from '../gameUI.js';
 import { gameState } from '../state.js';
 import * as getRulesModule from '../get_rules.js';
 import * as presetsModule from '../presets.js';
+// import { loadPreset } from '../presetMenu.js';
 import * as presetMenuModule from '../presetMenu.js';
+
 describe('bitTest()', () => {
   it('should return non-zero if a bit is set', () => {
     // 0b1010 is decimal 10. Bits are [1,0,1,0] from LSB to MSB
@@ -1021,4 +1023,175 @@ describe('setRule()', () => {
     // UI callback
     expect(gameUI.display_rule).toHaveBeenCalledWith(0, 77);
   });
+});
+
+
+describe('makeNewGame()', () => {
+  let loadPresetSpy;
+  let displayPresetFeaturesSpy;
+  let disableRetreatSpy;
+  let enableAdvanceSpy;
+  let timerResetSpy;
+  let timerStartSpy;
+
+  beforeEach(() => {
+    // We can reset or configure gameState for each test.
+    // We'll also provide a minimal mock for gameState.timer so we can confirm .reset() and .start().
+    gameState.timer = {
+      reset: vi.fn(),
+      start: vi.fn(),
+    };
+
+    // Clear out or define some default for these.
+    gameState.PRESET = 0;               // default example
+    gameState.CURRENT_MOVE = 5;
+    gameState.MOVE_COUNT = 5;
+    gameState.DRAG_COUNT = 2;
+    gameState.initial_state = {};       // you can populate to test the “PRESET == -1” logic
+    gameState.ROWS = 8;
+    gameState.COLS = 8;
+    gameState.CA_STATE_MATRIX = [];
+    gameState.RULES = [];
+    // etc.
+
+    // Provide a mock localStorage or spy on real localStorage
+    const store = {};
+    vi.spyOn(window.localStorage, 'getItem').mockImplementation((key) => store[key]);
+    vi.spyOn(window.localStorage, 'setItem').mockImplementation((key, val) => {
+      store[key] = val;
+    });
+
+    // Spy on or mock out some functions that makeNewGame calls
+    loadPresetSpy = vi.spyOn(presetMenuModule, 'loadPreset').mockImplementation(() => { });
+    displayPresetFeaturesSpy = vi.spyOn(gameUI, 'display_preset_features').mockImplementation(() => { });
+    disableRetreatSpy = vi.spyOn(gamelogic, 'disable_retreat_button').mockImplementation(() => { });
+    enableAdvanceSpy = vi.spyOn(gameUI, 'enable_advance_button').mockImplementation(() => { });
+
+    // Also spy on the timer
+    timerResetSpy = gameState.timer.reset;
+    timerStartSpy = gameState.timer.start;
+
+    document.body.innerHTML = `
+    <div id="entry_page">
+		<div id="entry_title">
+			<b>ChromaXen</b>
+		</div>
+		<div id="entry_continue_button" class="entry_button">
+			Continue
+		</div>
+
+		<div id="entry_game_button" class="entry_button">
+			New Game
+		</div>
+		<div id="entry_random_button" class="entry_button">
+			Random
+		</div>
+		<div id="entry_all_rules_button" class="entry_button">
+			All Rules
+		</div>
+	</div>
+    <div id="container">
+  <div id="game_header">
+    <div id="back_to_menu" class="button">&#x21DA; Menu</div>
+    <div id="game_title_display"></div>
+    <div id="game_desc_display"></div>
+    <div id="next_button" class="button">Next Level &#x21DB;</div>
+    <div id="prev_button" class="button">&#x21DA;</div>
+    <select id="preset_select_el"></select>
+  </div>
+
+  <div id="gameboard_container">
+    <table id="gameboard">
+    </table>
+
+  </div>
+
+  <div id="gameboard_overlay_container">
+    <div id="gameboard_overlay"></div>
+  </div>
+
+  <div id="game_footer">
+    <div id="update_button" class="button">Advance</div>
+    <div id="retreat_button" class="button">Retreat</div>
+    <div id="reset_button" class="button">Reset &#x21BA;</div>
+    <div id="random_button" class="button">Random</div>
+    <div id="moves_display">
+      <span>Moves: </span>
+      <span id="update_counter">0</span>
+    </div>
+    <div id="timer_display">
+      <span>Time: </span>
+      <span id="timer">00:00</span>
+    </div>
+    <div id="save_button" class="button">Save Game</div>
+    <div id="load_button" class="button">Load Game
+    </div>
+    <input type="file" id="load_game_input" accept=".json" style="display:none">
+    <div id="solve_button" class="button">Solve!</div>
+    <div id="dragndrop_style_display">Style: Swap</div>
+  </div>
+
+  <div id="win_screen_container">
+			<div id="win_screen">
+				<h1 id="win_screen_header">You win!</h1>
+				<h3 id="high_score_table_header"><b>Game 1 High Score</b></h3>
+				<table id="high_score_table">
+					<tr>
+						<th></th>
+						<th>Moves</th>
+						<th>Time</th>
+						<th>Name</th>
+					</tr>
+				</table>
+				<div id="win_screen_footer">
+					<div id="replay_button" class="button">Replay &#x21BA;</div>
+					<div id="next_level_button" class="button">Next &#x21DB;</div>
+				</div>
+			</div>
+		</div>
+
+		<div id="lose_screen_container">
+			<div id="lose_screen">
+				<h1 id="lose_screen_header">
+					You lose
+				</h1>
+				<div id="lose_screen_footer">
+					<div id="lose_replay_button" class="button">Replay</div>
+					<div id="lose_next_button" class="button">Next</div>
+				</div>
+			</div>
+		</div>
+  `;
+
+  });
+
+  afterEach(() => {
+    // If your Timer has a .stop() method, call it:
+    if (gameState.timer && typeof gameState.timer.stop === 'function') {
+      gameState.timer.stop();
+    }
+  });
+
+
+  it('loads random game if PRESET == -1 and is_random=true', () => {
+    // ARRANGE
+    // e.g. PRESET = -1 => triggers an if-block in your code
+    gameState.PRESET = -1;
+
+    // ACT
+    makeNewGame(true); // pass is_random = true
+
+    // ASSERT
+    // For the random scenario, your code calls loadPreset(gameState.PRESET) i.e. loadPreset(-1)
+    // double-check that happened:
+    expect(loadPresetSpy).toHaveBeenCalledTimes(1);
+    expect(loadPresetSpy).toHaveBeenCalledWith(-1);
+
+    // Timer & state checks
+    expect(timerResetSpy).toHaveBeenCalledTimes(1);
+    expect(timerStartSpy).toHaveBeenCalledTimes(1);
+
+    // etc.
+  });
+
 });
